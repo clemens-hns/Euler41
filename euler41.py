e3d6fe8807e1062f80ebd48e34a15bbe09d82ad4
Steve Clement <steve.clement@centurylink.com>
2016-04-14 11:08:25 -0400

    updated python file

--- euler41.py
+++ euler41.py
@@ -1,5 +1,28 @@
 # Euler 41
 
+def permutations(string, step = 0):
+
+    # if we've gotten to the end, print the permutation
+    if step == len(string):
+        string = "".join(string)
+	#print string
+
+    # everything to the right of step has not been swapped yet
+    for i in range(step, len(string)):
+
+        # copy the string (store as array)
+        string_copy = [character for character in string]
+
+        # swap the current index with the step
+        string_copy[step], string_copy[i] = string_copy[i], string_copy[step]
+
+        # recurse on the portion of the string that has not been swapped yet (now it's index will begin with step + 1)
+        permutations(string_copy, step + 1)
+	
+	string = "".join(string)
+	if(isPrime(int(string))):
+		print "Prime: ", string
+	
 def isPrime(n):
   if n == 2 or n == 3: return True
   if n < 2 or n%2 == 0: return False
@@ -14,4 +37,6 @@ def isPrime(n):
     f +=6
   return True    
   
-num_str = "123456789"
+
+num_str = "23456789"
+permutations(num_str)
